4d4aed112e4f31eb6731e8681ac35c38033c82ae
jinjiao <97863013+jinjiao007@users.noreply.github.com>
2025-06-02 14:30:03 +0800

    doro抽

---
+++ memes/dorochou/__init__.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from pathlib import Path
+
+from PIL.Image import Image as IMG
+from pil_utils import BuildImage
+
+from meme_generator import add_meme
+from meme_generator.utils import save_gif
+
+img_dir = Path(__file__).parent / "images"
+
+
+def dorochou(images: list[BuildImage], texts, args):
+    img = images[0].convert("RGBA").circle().resize((140, 140))  # 将用户头像转换为圆形
+    
+    # 0-10帧中头像的位置和大小
+    locs = [
+        (330, 44), (328, 45), (319, 45), 
+        (317, 50), (322, 48), (322, 48)        
+    ]
+
+    frames: list[IMG] = []
+    for i in range(6):
+        bg = BuildImage.open(img_dir / f"{i}.png")  # 打开背景图
+        frame = BuildImage.new("RGBA", bg.size, "white")  # 创建新帧
+        # 获取 locs 的位置和大小
+        frame.paste(img, locs[i], alpha=True)      
+        # 粘贴背景图
+        frame.paste(bg, alpha=True)       
+        frames.append(frame.image)  # 添加当前帧到帧列表
+    
+    return save_gif(frames, 0.1)  # 保存为 GIF，帧间隔 0.05 秒
+
+
+add_meme(
+    "dorochou",
+    dorochou,
+    min_images=1,
+    max_images=1,
+    keywords=["doro抽"],
+    date_created=datetime(2025, 6, 2),
+    date_modified=datetime(2025, 6, 2),
+)
